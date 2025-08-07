7e8674b038ae720d1c0d4a17a1a1dc10239f7e3a
MK <marmorakh2000@gmail.com>
2025-08-07 15:54:59 +0300

    C

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import ScrollableCards from "./ScrollableCards";
-import Courses from "./courses";;
+import Courses from "./Courses";;
 
 function ContactForm() {
   const [message, setMessage] = useState('');

945e0545003b877d6352b43338901123ae5ef447
MK <marmorakh2000@gmail.com>
2025-08-24 22:30:36 +0300

    I can write text here

--- src/PhotoCard.jsx
+++ src/PhotoCard.jsx
@@ -6,22 +6,22 @@ function PhotoCard({r, img}) {
       {r === 'r' ? (
         <div className="photo-card-container ">
           <div className="photo-card float-left">
-            <img src={img} className="photo-img" />
+            <img src={img} className="photo-img opacity-0" />
           </div>
           
-          <div className='flex-1 flex items-center justify-center '>
-            hi
+          <div className='flex-1 flex items-center justify-center text-[170%]'>
+            I can write text here
             </div>
 
         </div>
       ) : (
         <div className="photo-card-container">
         
-          <div className='flex-1 flex items-center justify-center '>
-            hi
+          <div className='flex-1 flex items-center justify-center text-[170%]'>
+            I can write text here
             </div>
         <div className="photo-card">
-            <img src={img} className="photo-img" />
+            <img src={img} className="photo-img opacity-0" />
         </div>
 
         </div>
